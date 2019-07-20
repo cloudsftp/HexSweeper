48f0c77d0c1953e965182905a4eae07f518aef78
Fabian Weik <fabian.weik@protonmail.com>
2019-07-20 03:27:09 +0200

    rewrite get() maxWidth

--- src/Logic/Field.kt
+++ src/Logic/Field.kt
@@ -2,17 +2,12 @@ package Logic
 
 class Field(val cells: Array<Array<Cell>>) : Iterable<Array<Cell>> {
 
+
     val maxWidth: Int
-    get() {
-        var max = 0
+    get() = cells.iterator().asSequence().maxBy { c -> c.size }!!.size
 
-        for (row in this)
-            if (max < row.size)
-                max = row.size
-
-        return max
-
-    }
+    val maxHeight: Int
+    get()  = cells.size
 
     override fun iterator(): Iterator<Array<Cell>> = cells.iterator()
 
