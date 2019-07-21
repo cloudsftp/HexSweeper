458a3682ce167e2fb27e7d38f4e4264a42f3d586
Fabian Weik <fabian.weik@protonmail.com>
2019-07-21 11:54:24 +0200

    introduced periodic method in Main

--- src/GUI/Main.kt
+++ src/GUI/Main.kt
@@ -1,23 +1,28 @@
 package GUI
 
 import Logic.*
-import kotlin.random.Random.Default.nextBoolean
+import java.util.*
+
+val scan = Scanner(System.`in`)
 
 fun main() {
     val n = 5
     val m = 6
 
+    val field = Field(n, m)
 
+    while (true) periodic(field)
 
-    val array = arrayOf(
-        arrayOf(Cell(false), Cell(true)),
-        arrayOf(Cell(false), Cell(false), Cell(true)),
-        arrayOf(Cell(true), Cell(false)))
+}
 
-    val field = Field(array)
-
-    field.cells[1][1].open()
+fun periodic(field: Field) {
 
     printField(field)
+
+    print("Zeile: ")
+    val i = scan.nextLine().trim().toInt()
+
+    print("Spalte: ")
+    val j = scan.nextLine().trim().toInt()
 
 }
