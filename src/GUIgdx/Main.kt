9061a51698fd9a3e6c74a6ef86c1715e7282b25f
Fabian Weik <fabian.weik@protonmail.com>
2019-07-21 16:10:33 +0200

    Now rendering something

--- src/GUIgdx/Main.kt
+++ src/GUIgdx/Main.kt
@@ -1,22 +1,35 @@
 package GUIgdx
 
 import com.badlogic.gdx.ApplicationListener
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class Main : ApplicationListener {
 
-    override fun create() {
+    internal lateinit var batch: SpriteBatch
+    internal lateinit var img: Texture
 
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("hexagon.jpg")
     }
 
     override fun render() {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
+    }
 
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
     }
 
     override fun pause() {
-
-    }
-
-    override fun resume() {
 
     }
 
@@ -24,7 +37,7 @@ class Main : ApplicationListener {
 
     }
 
-    override fun dispose() {
+    override fun resume() {
 
     }
 
