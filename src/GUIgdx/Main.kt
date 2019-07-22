468e7b80353f5b07df1b509909af2bacbdbe4ba4
Fabian Weik <fabian.weik@protonmail.com>
2019-07-23 01:13:28 +0200

    Now rendering Hexagon

--- src/GUIgdx/Main.kt
+++ src/GUIgdx/Main.kt
@@ -9,24 +9,28 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 class Main : ApplicationListener {
 
     internal lateinit var batch: SpriteBatch
-    internal lateinit var img: Texture
+    internal lateinit var hexagon: Texture
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("hexagon.jpg")
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         batch.begin()
-        batch.draw(img, 0f, 0f)
+
+        hexagon = Texture("hexagonsmall.png")
+
+        batch.draw(hexagon, 1f, 1f)
+
         batch.end()
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
+        hexagon.dispose()
     }
 
     override fun pause() {
