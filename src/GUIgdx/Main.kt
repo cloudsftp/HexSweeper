6ff95ea79deb2b65253509772f863503b47cea2f
Fabian Weik <fabian.weik@openmailbox.org>
2019-07-26 01:42:00 +0200

    Now rendering Sprites

--- src/GUIgdx/Main.kt
+++ src/GUIgdx/Main.kt
@@ -3,22 +3,31 @@ package GUIgdx
 import Logic.Field
 import com.badlogic.gdx.ApplicationListener
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class Main : ApplicationListener {
+class Main : ApplicationListener, InputProcessor {
+
+    internal lateinit var hexagonSprites: MutableList<MutableList<Sprite>>
 
     internal lateinit var batch: SpriteBatch
-    internal lateinit var hexagonFileHandle: FileHandle
+    internal lateinit var hexagonTexture: Texture
 
     override fun create() {
         batch = SpriteBatch()
 
-        hexagonFileHandle = Gdx.files.internal("res/hexagonsmall.png")
+        val hexagonFileHandle = Gdx.files.internal("res/hexagonsmall.png")
+        val hexagonPixmap = Pixmap(hexagonFileHandle)
+        hexagonTexture = Texture(hexagonPixmap)
+
+        hexagonPixmap.dispose()
+
     }
 
     override fun render() {
@@ -27,44 +36,68 @@ class Main : ApplicationListener {
 
         batch.begin()
 
-        batch.draw(fieldTexture(), 0f, 0f)
+        drawBackground()
+        drawField()
 
         batch.end()
+
     }
 
-    private fun fieldTexture(): Texture {
-        val pixmap = Pixmap(500, 500, Pixmap.Format.RGB565)
-        pixmap.setColor(Color.WHITE)
+    private fun drawBackground() {
+        val pixmap = Pixmap(Gdx.graphics.width, Gdx.graphics.height, Pixmap.Format.RGB565)
+        pixmap.setColor(Color.DARK_GRAY)
         pixmap.fill()
+        batch.draw(Texture(pixmap), 0f, 0f)
 
-        val field = Field(5, 3)
+    }
+
+
+    private fun drawField() {
+
+        val n = 7
+        val m = 4
+
+        val field = Field(n, m)
 
         var offsetX: Int
         val offsetY = 30
 
-        val hexagonPixmap = Pixmap(hexagonFileHandle)
+        hexagonSprites = mutableListOf()
 
         for (i in 0 until field.cells.size) {
+            hexagonSprites.add(mutableListOf())
+
             offsetX = if (i % 2 == 0) 77 else 0
 
-            for (j in 0 until field.cells[i].size)
-                pixmap.drawPixmap(hexagonPixmap, j * 153 + offsetX, i * 45 + offsetY)
+            for (j in 0 until field.cells[i].size) {
+                val hexagonSprite = Sprite(hexagonTexture)
+                hexagonSprite.setPosition(j * 153f + offsetX, i * 45f + offsetY)
+                hexagonSprite.draw(batch)
+
+                hexagonSprites[i].add(hexagonSprite)
+
+            }
 
         }
 
-        hexagonPixmap.dispose()
-
-        val texture = Texture(pixmap)
-        pixmap.dispose()
-
-        return texture
-
     }
+
 
     override fun dispose() {
         batch.dispose()
 
     }
+
+    override fun touchDown(p0: Int, p1: Int, p2: Int, p3: Int): Boolean {
+
+        print("Click")
+
+        return false
+
+    }
+
+
+    // useless
 
     override fun pause() {
 
@@ -76,6 +109,35 @@ class Main : ApplicationListener {
 
     override fun resume() {
 
+    }
+
+
+    override fun touchUp(p0: Int, p1: Int, p2: Int, p3: Int): Boolean {
+        return false
+    }
+
+    override fun touchDragged(p0: Int, p1: Int, p2: Int): Boolean {
+        return false
+    }
+
+    override fun mouseMoved(p0: Int, p1: Int): Boolean {
+        return false
+    }
+
+    override fun scrolled(p0: Int): Boolean {
+        return false
+    }
+
+    override fun keyTyped(p0: Char): Boolean {
+        return false
+    }
+
+    override fun keyDown(p0: Int): Boolean {
+        return false
+    }
+
+    override fun keyUp(p0: Int): Boolean {
+        return false
     }
 
 }
