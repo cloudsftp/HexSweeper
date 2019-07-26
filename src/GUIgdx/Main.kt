bab51bce48b3c07dded5ad7b70ef2bf6d387bef5
Fabian Weik <fabian.weik@openmailbox.org>
2019-07-26 11:33:35 +0200

    Clicks open Cells (in the background, not visible yet)

--- src/GUIgdx/Main.kt
+++ src/GUIgdx/Main.kt
@@ -13,6 +13,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class Main : ApplicationListener, InputProcessor {
 
+    val n = 7
+    val m = 4
+    val field = Field(n, m)
     internal lateinit var hexagonSprites: MutableList<MutableList<Sprite>>
 
     internal lateinit var batch: SpriteBatch
@@ -35,30 +38,26 @@ class Main : ApplicationListener, InputProcessor {
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        batch.begin()
-
         drawBackground()
         drawField()
-
-        batch.end()
 
     }
 
     private fun drawBackground() {
+        batch.begin()
+
         val pixmap = Pixmap(Gdx.graphics.width, Gdx.graphics.height, Pixmap.Format.RGB565)
         pixmap.setColor(Color.DARK_GRAY)
         pixmap.fill()
         batch.draw(Texture(pixmap), 0f, 0f)
 
+        batch.end()
+
     }
 
 
     private fun drawField() {
-
-        val n = 7
-        val m = 4
-
-        val field = Field(n, m)
+        batch.begin()
 
         var offsetX: Int
         val offsetY = 60
@@ -82,6 +81,8 @@ class Main : ApplicationListener, InputProcessor {
 
         }
 
+        batch.end()
+
     }
 
 
@@ -91,8 +92,25 @@ class Main : ApplicationListener, InputProcessor {
     }
 
     override fun touchDown(p0: Int, p1: Int, p2: Int, p3: Int): Boolean {
+        println("Click at $p0, $p1, $p2, $p3")
 
-        println("Click")
+        fun clickInSprite(sprite: Sprite, x: Int, y: Int)
+            = x > sprite.x && x < sprite.x + sprite.width
+                && y > sprite.y && y < sprite.y + sprite.height
+
+        for (row in hexagonSprites)
+            for (sprite in row)
+                if (clickInSprite(sprite, p0, p1)) {
+                    val i = hexagonSprites.indexOf(row)
+                    val j = row.indexOf(sprite)
+
+                    println("$i, $j")
+
+                    field.open(i, j)
+
+                }
+
+        drawField()
 
         return true
 
