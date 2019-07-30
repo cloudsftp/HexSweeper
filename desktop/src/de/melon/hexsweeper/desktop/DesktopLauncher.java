5d80ec586eb96ae4aa6a0bec14b7fb3de707b70a
Fabian Weik <fabian.weik@protonmail.com>
2019-07-30 14:46:10 +0200

    New structure

---
+++ desktop/src/de/melon/hexsweeper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.melon.hexsweeper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.melon.hexsweeper.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
