ce6662d7b60f183092e70144e64d206813b5d477
Fabian Weik <fabian.weik@protonmail.com>
2019-08-03 16:25:32 +0200

    Travis build fix (ios)

--- ios/src/de/melon/hexsweeper/IOSLauncher.java
+++ ios/src/de/melon/hexsweeper/IOSLauncher.java
@@ -5,13 +5,13 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import de.melon.hexsweeper.GUI.Game;
+import de.melon.hexsweeper.GUI.GameRenderer;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Game(), config);
+        return new IOSApplication(new GameRenderer(), config);
     }
 
     public static void main(String[] argv) {
